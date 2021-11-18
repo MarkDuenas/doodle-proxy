4eea40c3fce2641fe2432e9e8a8350bdb686d85a
Mark Duenas <markgdlduenas@gmail.com>
2021-11-18 12:32:09 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+const PORT = 8000;
+
+const API_URL = "https://www.google.com/doodles/json";
+
+app.use(express.json());
+
+app.get("/get/doodle/:year/:month", async (req, res) => {
+  try {
+    let result = await axios.get(
+      `${API_URL}/${req.params.year}/${req.params.month}`
+    );
+    // console.log(result.headers);
+    res.status(200).json(result.data);
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.listen(PORT, () => console.log(`Server up on port ${PORT}`));
